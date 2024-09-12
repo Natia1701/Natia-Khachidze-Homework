c43d81e6b65e5abb2ec848f0e6640e3a2026c762
Nuno Pereira <82775049+npereira89@users.noreply.github.com>
2024-09-12 18:34:26 +0100

    Fix value of variable to input

--- task4.py
+++ task4.py
@@ -1,3 +1,3 @@
-temperature  = input ("enter temperature in Celsius ")
+temperature = int(input ("enter temperature in Celsius "))
 farengheit = int(temperature) *1.8+32
 print ("The temperature in farengheit is " +str(farengheit)+ ".")
